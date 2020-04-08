35073febabebd568e390d4523f6b5ee9958ca64d
real-person <60947319+real-person@users.noreply.github.com>
2020-04-08 17:03:46 -0230

    Moved all code into function Refactored for consistency

--- get_dataset.py
+++ get_dataset.py
@@ -7,46 +7,34 @@ from config import config
 from histogram_equalization import equalize_dataset
 from normalization import normalize_dataset
 from clahe import clahe_dataset
-from selective_equalization import selective_clahe_dataset
 from gamma_correction import gamma_correct_dataset
 
 
-annType = config['annTypes'][2]      # specify type here
-prefix = 'person_keypoints' if annType == 'keypoints' else 'instances'
-
-# Setup data paths
-dataDir = config['dataDir']
-dataType = config['dataType']
-
-annDir = f'{dataDir}/annotations'
-
-imDir = f'{dataDir}/{dataType}'
-eqDir = f'{dataDir}/Equalized'
-normDir = f'{dataDir}/Normalized'
-
-annZipFile = f'{dataDir}/instances_{dataType}.zip'
-annZipFile_foot = f'{dataDir}/person_keypoints_val2017_foot_v1.zip'
-imZipFile = f'{dataDir}/images_{dataType}.zip'
-annFile = f'{annDir}/{prefix}_{dataType}.json'
-annFile_foot = f'{annDir}/person_keypoints_val2017_foot_v1.json'
-annURL = f'http://images.cocodataset.org/annotations/annotations_train{dataType}.zip'
-annURL_foot = f'http://posefs1.perception.cs.cmu.edu/OpenPose/datasets/foot/person_keypoints_val2017_foot_v1.zip'
-imURL = f'http://images.cocodataset.org/zips/{dataType}.zip'
-
-
 def get_dataset():
+    # Type of annotations required
+    annType = config['annType']
+    prefix = 'person_keypoints' if annType == 'keypoints' else 'instances'
+
+    # Setup data paths
+    dataDir = config['dataDir']
+    dataType = config['dataType']
+
+    annDir = f'{dataDir}/annotations'
+    annZipFile = f'{dataDir}/instances_{dataType}.zip'
+    annFile = f'{annDir}/{prefix}_{dataType}.json'
+    annURL = f'http://images.cocodataset.org/annotations/annotations_train{dataType}.zip'
+
+    annZipFile_foot = f'{dataDir}/person_keypoints_val2017_foot_v1.zip'
+    annFile_foot = f'{annDir}/person_keypoints_val2017_foot_v1.json'
+    annURL_foot = f'http://posefs1.perception.cs.cmu.edu/OpenPose/datasets/foot/person_keypoints_val2017_foot_v1.zip'
+
+    imDir = f'{dataDir}/{dataType}'
+    imZipFile = f'{dataDir}/images_{dataType}.zip'
+    imURL = f'http://images.cocodataset.org/zips/{dataType}.zip'
+    
     # Create folder for the annotations if it doesnt already exist
     if not os.path.exists(annDir):
         os.makedirs(annDir)
-    # Create folder for the original dataset if it doesnt already exist
-    if not os.path.exists(imDir):
-        os.makedirs(imDir)
-    # Create folder for the equalized dataset if it doesnt already exist
-    if not os.path.exists(eqDir):
-        os.mkdir(eqDir)
-    # Create folder for the normalized dataset if it doesnt already exist
-    if not os.path.exists(normDir):
-        os.mkdir(normDir)
 
     # Download body annotations if not available locally
     if not os.path.exists(annFile):
@@ -76,6 +64,11 @@ def get_dataset():
         print("... done unzipping")
     print("Will use annotations in " + annFile_foot)
 
+    # Create folder for COCO image dataset if it doesnt already exist
+    if not os.path.exists(imDir):
+        os.makedirs(imDir)
+
+    # Download COCO image dataset if not available locally
     if len(os.listdir(imDir)) == 0:
         if not os.path.exists(imZipFile):
             print("Downloading zipped images to " + imZipFile + " ...")
@@ -90,41 +83,36 @@ def get_dataset():
     print("Will use images in " + imDir)
 
     if config['Equalize']:
+        eqDir = f'{dataDir}/Equalized'
+        # Create folder for the equalized dataset if it doesnt already exist
+        if not os.path.exists(eqDir):
+            os.mkdir(eqDir)
         if len(os.listdir(eqDir)) == 0:
             equalize_dataset(imDir, eqDir)
 
     if config['Normalize']:
+        normDir = f'{dataDir}/Normalized'
+        # Create folder for the normalized dataset if it doesnt already exist
+        if not os.path.exists(normDir):
+            os.mkdir(normDir)
         if len(os.listdir(normDir)) == 0:
             normalize_dataset(imDir, normDir)
 
     if config['CLAHE']:
-        for clip_limit in config['CLAHE clip_limit']:
-            for grid_size in config['CLAHE grid_size']:
-                claheDir = f'{dataDir}/CLAHE_{clip_limit}_{grid_size}'
-                if not os.path.exists(claheDir):
-                    os.makedirs(claheDir)
-                if len(os.listdir(claheDir)) == 0:
-                    clahe_dataset(imDir, claheDir, clip_limit, grid_size)
-
-    if config['CLAHE']:
-        clip_limit = config['CLAHE clip_limit'][0]
-        for grid_size in config['CLAHE grid_size'][:2]:
-            mean_thresh = config['Mean intensity threshold']
-            claheDir = f'{dataDir}/Selective_CLAHE_{clip_limit}_{grid_size}_{mean_thresh}'
-            if not os.path.exists(claheDir):
-                os.makedirs(claheDir)
-            if len(os.listdir(claheDir)) == 0:
-                selective_clahe_dataset(imDir, claheDir, clip_limit, grid_size)
+        if not config['CLAHE threshold']:
+            config['CLAHE threshold'] = (255, )
+        for cl in config['CLAHE clip_limit']:
+            for gs in config['CLAHE grid_size']:
+                for thresh in config['CLAHE threshold']:
+                    claheDir = f'{dataDir}/CLAHE_CL{cl}_GS{gs}_T{thresh}'
+                    if not os.path.exists(claheDir):
+                        os.makedirs(claheDir)
+                    if len(os.listdir(claheDir)) == 0:
+                        clahe_dataset(imDir, claheDir, cl, gs, thresh)
 
     if config['Gamma']:
-        for gamma in config['Gamma values']:
-            gammaDir = f'{dataDir}/Gamma_{gamma}'
-            if not os.path.exists(gammaDir):
-                os.makedirs(gammaDir)
-            if len(os.listdir(gammaDir)) == 0:
-                gamma_correct_dataset(imDir, gammaDir, gamma)
-
-    if config['Selective Gamma']:
+        if not config['Gamma threshold']:
+            config['Gamma threshold'] = (255, )
         for gamma in config['Gamma values']:
             for thresh in config['Gamma threshold']:
                 gammaDir = f'{dataDir}/Gamma_{gamma}_thresh_{thresh}'
