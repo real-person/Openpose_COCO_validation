fd980751b862db4aee7669609bf93a2bc9a1d4ca
real-person <60947319+real-person@users.noreply.github.com>
2020-04-10 10:58:56 -0230

    Added net resolution and scale number configurations

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 # Folders to test must be either None or iterables
 config = {
     'annType': 'keypoints',
-    'dataDir': 'Path/to/where/you/want/dataset',
+    'dataDir': 'C:/Users/marsh/Pictures/COCO',
     'dataType': 'val2017',
-    'model_folder': 'path/to/openpose/caffe_model',
-    'openpose_folder': 'Path/to/openpose/folder',
+    'model_folder': 'C:/Users/marsh/Documents/GitHub/openpose_train/experimental_models/1_25BSuperModel11FullVGG/body_25b',
+    'openpose_folder': 'C:/Users/marsh/Desktop/PythonProjects/openpose-1.5.1-binaries-win64-gpu-python-flir-3d_recommended/openpose',
     'Equalize': False,
     'Normalize': False,
     'CLAHE': False,
@@ -17,5 +17,13 @@ config = {
     'Gamma values': [1.5, 2],
     'Gamma threshold': (60, 80),
     'Foot': False,
-    'Folders to test': ['val2017', ]
+    'Folders to test': ['val2017', ],
+    'Scale_number': 1   # 1 for less memory consumption, 4 for maximum accuracy
+}
+
+net_resolutions = {
+    '1_25BSuperModel11FullVGG': {1: "-1x480", 4: "1712x960"},
+    '100_135AlmostSameBatchAllGPUs': {1: "-1x480", 4: "1712x960"},
+    '1_25BBkg': {1: "-1x368", 4: "1312x736"},
+    'body_25b': {1: "-1x368", 4: "1312x736"}
 }
--- get_coco_jsons.sh
+++ get_coco_jsons.sh
@@ -13,12 +13,13 @@ OPENPOSE_MODEL=$(basename ${SHARED_FOLDER})
 OPENPOSE_MODEL=${OPENPOSE_MODEL^^}
 EXPERIMENT=$(basename $(dirname ${SHARED_FOLDER}))
 
-
+# Folder where openpose is installed 
 OPENPOSE_FOLDER=$3
-# NUMBER_FOLDER=135
-# EXPERIMENT=body_25
-# Body/Foot
-# IMAGE_DIR="C:/Users/marsh/Pictures/COCO/val2017"
+
+# Resolution settings
+SCALE_NUMBER=$4
+SCALE_GAP=$5
+NET_RESOLUTION=$6
 
 # Face
 # IMAGE_DIR_FRGC="/home/gines/devel/images/frgc_val/"
@@ -28,25 +29,14 @@ OPENPOSE_FOLDER=$3
 # IMAGE_DIR_HAND_DOME="/home/gines/devel/images/hand_dome_val/"
 # IMAGE_DIR_HAND_MPII="/home/gines/devel/images/hand_mpii_val/"
 
-echo "Common parameters to both files a_*.sh and b_*.sh"
-# SHARED_FOLDER=C:/Users/marsh/Documents/GitHub/openpose_train/experimental_models/${EXPERIMENT}/body_${NUMBER_FOLDER}/
-# SHARED_FOLDER=C:/Users/marsh/Documents/GitHub/openpose_train/experimental_models/${EXPERIMENT}/body_${NUMBER_FOLDER}b/
-# SHARED_FOLDER=/media/posefs3b/Users/gines/openpose_train/training_results/${EXPERIMENT}/pose/body_${NUMBER_FOLDER}d/
-# SHARED_FOLDER=/media/posefs3b/Users/gines/openpose_train/training_results/${EXPERIMENT}/pose/body_${NUMBER_FOLDER}e/
-# SHARED_FOLDER=/media/posefs3b/Users/gines/openpose_train/training_results/${EXPERIMENT}/pose/body_${NUMBER_FOLDER}n/
-# SHARED_FOLDER=/media/posefs3b/Users/gines/openpose_train/training_results/${EXPERIMENT}/pose/body_${NUMBER_FOLDER}_x2/
-echo " "
 
 echo "Paths"
-# OPENPOSE_MODEL=BODY_${NUMBER_FOLDER}
-# OPENPOSE_MODEL=BODY_${NUMBER_FOLDER}B
-# OPENPOSE_MODEL=BODY_${NUMBER_FOLDER}E
-# OPENPOSE_FOLDER=C:/Users/marsh/Documents/School/Term_8/Engineering_in_Medicine/Motion_Tracking_Project/openpose-1.5.1-binaries-win64-gpu-python-flir-3d_recommended/openpose
+
 # Body
-JSON_FOLDER_1=${SHARED_FOLDER}1scale/
+JSON_FOLDER=${SHARED_FOLDER}${SCALE_NUMBER}scale/
 # JSON_FOLDER_4=${SHARED_FOLDER}4scales/
 # Foot
-JSON_FOLDER_1_foot=${SHARED_FOLDER}foot_1scale/
+JSON_FOLDER_foot=${SHARED_FOLDER}foot_${SCALE_NUMBER}scale/
 # JSON_FOLDER_4_foot=${SHARED_FOLDER}foot_4scales/
 # Face
 # JSON_FOLDER_1_frgc=${SHARED_FOLDER}frgc_1scale/
@@ -63,10 +53,10 @@ JSON_FOLDER_1_foot=${SHARED_FOLDER}foot_1scale/
 
 # Create folders
 # Body
-mkdir $JSON_FOLDER_1
+mkdir $JSON_FOLDER
 # mkdir $JSON_FOLDER_4
 # Foot
-mkdir $JSON_FOLDER_1_foot
+mkdir $JSON_FOLDER_foot
 # mkdir $JSON_FOLDER_4_foot
 # Face
 # mkdir $JSON_FOLDER_1_frgc
@@ -80,15 +70,8 @@ mkdir $JSON_FOLDER_1_foot
 # mkdir $JSON_FOLDER_4_hand_dome
 # mkdir $JSON_FOLDER_1_hand_mpii
 # mkdir $JSON_FOLDER_4_hand_mpii
-echo " "
-
-# echo "Sleeping for 8h..."
-# sleep 27000
-# echo "Awaken!"
 
 
-
-# Different code than a_*.sh
 echo "Running OpenPose for each model"
 MODEL_FOLDER=$(dirname $(dirname ${SHARED_FOLDER}))/
 pwd
@@ -97,36 +80,35 @@ echo $PWD
 
 # Sorted in natural order (NAT sort)
 for modelPath in `ls -v ${SHARED_FOLDER}*.caffemodel`; do
-# Not NAT sort
-# for modelPath in ${SHARED_FOLDER}*.caffemodel; do
     prototxtPath=$(dirname ${modelPath})/pose_deploy.prototxt
     modelName=$(basename ${modelPath})
     echo "Processing $modelName in $EXPERIMENT"
 
-    finalJsonFile1=${JSON_FOLDER_1}${modelName}_1_${ImType}.json
-    finalJsonFile1_foot=${JSON_FOLDER_1_foot}${modelName}_1_${ImType}.json
-    temporaryJsonFile1=C:/Users/marsh/Desktop/temporaryJson_${EXPERIMENT}_${modelName}_1.json
-    temporaryJsonFile1Foot=C:/Users/marsh/Desktop/temporaryJson_${EXPERIMENT}_${modelName}_1_foot.json
+    finalJsonFile=${JSON_FOLDER}${modelName}_${SCALE_NUMBER}_${ImType}.json
+    finalJsonFile_foot=${JSON_FOLDER_foot}${modelName}_${SCALE_NUMBER}_${ImType}.json
+    temporaryJsonFile=C:/Users/marsh/Desktop/temporaryJson_${EXPERIMENT}_${modelName}_${SCALE_NUMBER}.json
+    temporaryJsonFileFoot=C:/Users/marsh/Desktop/temporaryJson_${EXPERIMENT}_${modelName}_${SCALE_NUMBER}_foot.json
 
-    OP_COMAND="bin/OpenPoseDemo.exe --model_pose ${OPENPOSE_MODEL} --prototxt_path ${prototxtPath} --caffemodel_path ${modelPath} --render_pose 0 --display 0 --cli_verbose 0.2"
-    OP_COMAND_1SCALE="${OP_COMAND} --write_coco_json ${temporaryJsonFile1} --num_gpu -1 "
-    OP_COMAND_4SCALES="${OP_COMAND} --write_coco_json ${temporaryJsonFile4} --num_gpu -1"
+    OP_COMAND="bin/OpenPoseDemo.exe --model_pose ${OPENPOSE_MODEL} --prototxt_path ${prototxtPath} "
+    OP_COMAND+="--caffemodel_path ${modelPath} --render_pose 0 --display 0 --cli_verbose 0.2 "
+    OP_COMAND+="--write_coco_json ${temporaryJsonFile} --num_gpu -1 --image_dir ${IMAGE_DIR} "
+    OP_COMAND+="--write_coco_json_variants 3 --model_folder "" --net_resolution ${NET_RESOLUTION} "
+    OP_COMAND+="--scale_number ${SCALE_NUMBER} --scale_gap ${SCALE_GAP}"
 
     # Body/foot 1 scale
     echo "Processing bodies/feet..."
     
-
-    if [ -f $finalJsonFile1 ]; then
-        echo "1-scale body/foot model already exists."
+    if [ -f $finalJsonFile ]; then
+        echo "${SCALE_NUMBER}-scale body/foot model already exists."
     else
-        # Body/foot processing
-        $OP_COMAND_1SCALE --image_dir ${IMAGE_DIR} --write_coco_json_variants 3 --model_folder "" # --net_resolution -1x480
+        # Run Openpose for body/foot processing
+        $OP_COMAND
         echo "Moving Temp JSON file... "
         # Move JSON to NAS after finished
-        mv ${temporaryJsonFile1} ${finalJsonFile1}
-        mv ${temporaryJsonFile1Foot} ${finalJsonFile1_foot}
-    fi  
-  
+        mv ${temporaryJsonFile} ${finalJsonFile}
+        mv ${temporaryJsonFileFoot} ${finalJsonFile_foot}
+    fi 
+
 done
 
 echo "Finished! Exiting script in 10 seconds..."
